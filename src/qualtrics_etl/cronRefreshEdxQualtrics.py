53a4c07e6d4da7a0c990ed47f62eda45d2484802
Alex Kindel <atkindel@stanford.edu>
2015-05-27 11:41:33 -0700

    Nevermind, the warnings can stay.

--- src/qualtrics_etl/cronRefreshEdxQualtrics.py
+++ src/qualtrics_etl/cronRefreshEdxQualtrics.py
@@ -1,8 +1,6 @@
 from surveyextractor import QualtricsExtractor
-from warnings import filterwarnings
 
 # Script for scheduling regular EdxQualtrics updates
-filterwarnings(action='ignore', category=Warning)
 qe = QualtricsExtractor()
 qe.setupDB()
 qe.loadSurveyMetadata()
