cf35dda460b77246fdfee87175a092312ff06dc5
Alex Kindel <atkindel@stanford.edu>
2015-05-27 13:06:18 -0700

    Fixed typo in cron script.

--- src/qualtrics_etl/cronRefreshEdxQualtrics.py
+++ src/qualtrics_etl/cronRefreshEdxQualtrics.py
@@ -4,6 +4,7 @@ import getopt, sys
 # Script for scheduling regular EdxQualtrics updates
 # Usage for cron should be "cronRefreshEdxQualtrics.py -m -s -r"
 
+qe = QualtricsExtractor()
 opts, args = getopt.getopt(sys.argv[1:], 'amsr', ['--reset', '--loadmeta', '--loadsurveys', '--loadresponses'])
 for opt, arg in opts:
     if opt in ('-a', '--reset'):
