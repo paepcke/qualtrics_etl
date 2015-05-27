1659d7ecec347d08da067e24421450b0123631fa
Alex Kindel <atkindel@stanford.edu>
2015-05-27 11:30:48 -0700

    Cron job modification to directories.

---
+++ src/qualtrics_etl/cronRefreshEdxQualtrics.py
@@ -0,0 +1,8 @@
+from surveyextractor import QualtricsExtractor
+
+# Script for scheduling regular EdxQualtrics updates
+qe = QualtricsExtractor()
+qe.setupDB()
+qe.loadSurveyMetadata()
+qe.loadSurveyData()
+qe.loadResponseData()
