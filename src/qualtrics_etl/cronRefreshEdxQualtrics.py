67f98aa8f910819e7e6e90def2b44a4fc0ba1bef
Alex Kindel <atkindel@stanford.edu>
2015-05-27 11:35:38 -0700

    Filtering warnings in cron script.

--- src/qualtrics_etl/cronRefreshEdxQualtrics.py
+++ src/qualtrics_etl/cronRefreshEdxQualtrics.py
@@ -1,6 +1,8 @@
 from surveyextractor import QualtricsExtractor
+from warnings import filterwarnings
 
 # Script for scheduling regular EdxQualtrics updates
+filterwarnings('ignore', '*')
 qe = QualtricsExtractor()
 qe.setupDB()
 qe.loadSurveyMetadata()
