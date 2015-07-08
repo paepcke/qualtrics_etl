3bf483eb001ce4f3a604c965deca3de0cb1f6f90
Alex Kindel <atkindel@stanford.edu>
2015-07-08 16:21:32 -0700

    typo fix

--- src/qualtrics_etl/surveyextractor.py
+++ src/qualtrics_etl/surveyextractor.py
@@ -399,9 +399,14 @@ class QualtricsExtractor(MySQLDB):
             rm['a'] = rs.pop('a', 'NULL')
             rm['UID'] = rs.pop('uid', 'NULL')
             rm['userid'] = rs.pop('user_id', 'NULL') #NOTE: Not transformed, use unclear
+            if rm['SurveyID'] = 'SV_3lSGEka3hgs74fX':
+                print rm['a']
+                print rm['UID']
             if(len(rm['UID']) >= 40):
+                print rm['UID']
                 rm['anon_screen_name'] = rm['UID']
             elif (len(rm['a']) >= 32):
+                print rm['a']
                 rm['anon_screen_name'] = self.__getAnonUserID(rm['a'])
             if (len(rm['IPAddress']) in range(8,16)):
                 rm['Country'] = self.lookup.lookupIP(rm['IPAddress'])[1]
