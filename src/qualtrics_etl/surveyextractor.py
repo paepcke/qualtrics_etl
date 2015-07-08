63284889e09c8e60a8d7027e9c8c538d020345ab
Alex Kindel <atkindel@stanford.edu>
2015-07-08 15:11:54 -0700

    Reordering filter a bit

--- src/qualtrics_etl/surveyextractor.py
+++ src/qualtrics_etl/surveyextractor.py
@@ -399,10 +399,10 @@ class QualtricsExtractor(MySQLDB):
             rm['a'] = rs.pop('a', 'NULL')
             rm['UID'] = rs.pop('uid', 'NULL')
             rm['userid'] = rs.pop('user_id', 'NULL') #NOTE: Not transformed, use unclear
-            if (len(rm['a'].strip()) >= 32):
-                rm['anon_screen_name'] = self.__getAnonUserID(rm['a'].strip())
-            elif(len(rm['UID'].strip()) >= 40):
+            if(len(rm['UID'].strip()) >= 40):
                 rm['anon_screen_name'] = rm['UID'].strip()
+            elif (len(rm['a'].strip()) >= 32):
+                rm['anon_screen_name'] = self.__getAnonUserID(rm['a'].strip())
             if (len(rm['IPAddress']) in range(8,16)):
                 rm['Country'] = self.lookup.lookupIP(rm['IPAddress'])[1]
             elif (len(rm['UID']) in range(8,16)):
