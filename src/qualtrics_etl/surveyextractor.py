b8bca9aeb68d1e1ebf39b8bf075a878070c315b6
Alex Kindel <atkindel@stanford.edu>
2015-07-09 17:05:57 -0700

    Fix to column header issue during DB insert

--- src/qualtrics_etl/surveyextractor.py
+++ src/qualtrics_etl/surveyextractor.py
@@ -446,7 +446,7 @@ class QualtricsExtractor(MySQLDB):
         represented as a list of dicts mapping column names to values.
         '''
         try:
-            columns = tuple(data[200].keys())
+            columns = tuple(data[20].keys())
             table = []
             # logging.info("     " + ", ".join(columns))
             for row in data:
